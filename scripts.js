b6c26df6230de6b2448cfe0a776fca44cd166a2a
ActuallyIsJesse <jesse.al.lord@gmail.com>
2022-11-09 14:34:36 -0500

    Add scaling for rows/columns according to grid size

--- scripts.js
+++ scripts.js
@@ -1,17 +1,28 @@
-let gridSize = 16;
+let selectedColor = "Black";
 const gridWrapper = document.querySelector('#grid-wrapper');
 
-function drawGrid() {
+function drawGrid(gridSize) {
+    while (gridWrapper.firstChild) {
+     gridWrapper.removeChild(gridWrapper.firstChild); // Clear previous grid from screen
+    }
     for (let i = 0; i < gridSize; i++) { //Draw Rows
         let gridRow = document.createElement(`div`);
         gridRow.classList.add('grid-row');
         gridWrapper.appendChild(gridRow);
+        gridRow.style.height = `${((768 / gridSize)-2) + "px"}`; // Determine how high each row should be and subtract border height
+        console.log(768 / gridSize);
         for (let j = 0; j < gridSize; j++) { // Draw Columns
             let gridCol = document.createElement(`div`);
+            gridCol.setAttribute('id', `${i}, ${j}`);
+            gridCol.style.width = `${(768 / gridSize) + "px"}`; // Determine how wide each box should be based on grid size
+            gridCol.addEventListener('mouseover', function (e) {
+                e.target.style.background = 'black';
+              });
             gridCol.classList.add('grid-item');
             gridRow.appendChild(gridCol);
         }
     }
+    const gridCells = document.querySelector('#grid')
 }
 
-drawGrid();
+drawGrid(16);
